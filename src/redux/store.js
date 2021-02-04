d85d14f1c41c8d5e33de64c98a12854bb4bb2a97
Nicholas Ochs <nick.ochs@yahoo.com>
2021-02-03 19:17:20 -0500

    Adds redux dev tool into createStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -49,6 +49,6 @@ const reducer = (state = defaultState, action) => {
     };
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
