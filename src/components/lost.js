2daddc12c1eb871c2e9826ee35f2fd8e758124f8
Nicholas Ochs <nick.ochs@yahoo.com>
2021-02-05 22:31:49 -0500

    Adds user answer and correct answer to Lost component

--- src/components/lost.js
+++ src/components/lost.js
@@ -4,10 +4,11 @@ import store from '../redux/store';
 function Lost() {
     
     return (
-        <div className="text-3xl text-center">
+        <div className="text-3xl text-center block">
             <p>You lost on round {store.getState().question.length}</p>
+            <p>You entered: {store.getState().answer.join(', ')}</p>
+            <p>The correct answer was: {store.getState().question.join(', ')}</p>
             <button type="button" className="bg-blue-600 mt-2 mb-2" onClick={() => store.dispatch({ type: 'RESTART' })}>Click here to restart</button>
-            <p>Placeholder for leaderboard form</p>
         </div>
     );
 
